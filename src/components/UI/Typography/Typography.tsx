f19dd6f00b5ead40906db67e4b716b602c67ab8c
otavio-cipriano <otavio.b.cipriano@gmail.com>
2023-04-13 11:50:25 -0300

    updated typography

--- src/components/UI/Typography/Typography.tsx
+++ src/components/UI/Typography/Typography.tsx
@@ -4,13 +4,16 @@ import * as style from './Typography.styled'
 type TagVariants = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'
 
 interface ITypography {
-    tag?: TagVariants,
-    size?: keyof typeof FONT.sizes,
-    children: React.ReactNode
+  tag?: TagVariants,
+  size?: keyof typeof FONT.sizes,
+  color?: keyof typeof FONT.colors,
+  children: React.ReactNode
 }
 
-export default function Typography({children, tag = 'p', size, ...props}:ITypography) {
+export default function Typography({ children, tag = 'p', size, color, ...props }: ITypography) {
   return (
-    <style.Typography tag={tag} size={size}>{children}</style.Typography>
+    <style.Typography tag={tag} size={size} color={color}>
+      {children}
+    </style.Typography>
   )
 }
