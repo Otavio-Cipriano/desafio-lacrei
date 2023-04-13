e38da0e52e4af01ee496bd810c7dda1d8f2e49b4
otavio-cipriano <otavio.b.cipriano@gmail.com>
2023-04-13 11:25:52 -0300

    Created a Typography component, Added Font in styles\Variables.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
-import Button from "@/components/UI/Button/Button";
-import Container from "@/components/UI/Container/Container";
+import { Button, Container, Typography } from "@/components/UI";
+import NextImage from "next/image";
+
+import styled from 'styled-components'
 
 export default function Home() {
   return (
     <>
       <main>
         <Container>
-          <Button variant="primary">Pessoa Usuária</Button>
+          <div>
+            <div>
+              <Typography tag="h2" size="xxlarge">Boas Vindas a Lacrei Saúde</Typography>
+              <p>
+                Uma plataforma segura e acolhedora para comunidade LGBTQIAPN+
+              </p>
+              <Button variant="primary">Pessoa Usuária</Button>
+              <Button variant="secondary">Profissional</Button>
+            </div>
+            <div>
+              <NextImage src={'/rafiki1.svg'} alt="Doutora Acenando"  fill/>
+            </div>
+          </div>
         </Container>
       </main>
     </>
   )
 }
+
+const Row = styled.div`
+  display: flex;
+`
--- src/styles/Variables.ts
+++ src/styles/Variables.ts
@@ -1,6 +1,19 @@
 export const colors = {
     green: 'rgb(1, 135, 98)',
     gray: 'rgb(238, 238, 238)',
+    darkGray: 'rgb(81, 81, 81)',
     black: 'rgb(31, 31, 31)',
     white: 'rgb(255, 255, 255)'
+}
+
+
+export const FONT = {
+    colors: {...colors},
+    sizes: {
+        small: '0.8rem',
+        regular: '1rem',
+        large: '1.5rem',
+        xlarge: '2rem',
+        xxlarge: '3rem'
+    }
 }
